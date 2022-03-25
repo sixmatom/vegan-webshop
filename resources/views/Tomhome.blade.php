2693c72d29ee7a34a96b81b67f939e8c3f8c0f9b
Tom Sixma <sixmatom@gmail.com>
2022-03-25 14:22:36 +0100

    added tomproducts

---
+++ resources/views/Tomhome.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+    table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    td,
+    th {
+        border: 1px solid #dddddd;
+        text-align: center;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #dddddd;
+    }
+    </style>
+</head>
+
+<body>
+    <div class=container style=text-align:center>
+        <table>
+            <tr>
+                
+                <th>name</th>
+                <th>color</th>
+            </tr>
+            <?php 
+    use App\Models\Tomproducts;
+
+    foreach (Tomproducts::all() as $product): ?>
+
+            <tr>
+                <td> <?php echo $product->name;?></td>
+
+                <td> <?php echo $product->color;?> </td>
+            </tr>
+
+            <?php endforeach;  ?>
+        </table>
+    </div>
