e3790cd401b9500967698f56567e96f1c7782c86
Tom Sixma <sixmatom@gmail.com>
2022-03-30 15:16:22 +0200

    adjusted link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
- <p align="center"><a href="http://127.0.0.1:8000/Tomhome">Tom's Website</a></p>
+ <p align="center"><a href="/Tomhome">Tom's Website</a></p>
  <p align="center">
      <a href="">Robert Jan's Website</a>
  </p>
